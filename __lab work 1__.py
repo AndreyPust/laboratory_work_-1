e174630ed11eead8d2aa4aa5b0c383fe9b800ab9
Andrey <pustakovandrej@gmail.com>
2023-11-06 21:52:14 +0300

    add beginning programm

--- __lab work 1__.py
+++ __lab work 1__.py
@@ -1,1 +1,6 @@
-print ("The World!")
+# the program for solving the quadratic equation
+from math import sqrt
+print("enter the a, b, c:")
+a = float(input())
+b = float(input())
+c = float(input())
