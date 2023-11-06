3fa72b0fd7c09b58ecc68acc0a941f1790dbfa2a
Andrey <pustakovandrej@gmail.com>
2023-11-06 22:16:36 +0300

    editing a hearth for the pep8 standard

--- __lab work 1__.py
+++ __lab work 1__.py
@@ -1,12 +1,24 @@
 # the program for solving the quadratic equation
-from math import sqrt #using libraries
+from math import sqrt
+
+# using libraries
+
 print("enter the a:")
 a = float(input())
 print("enter the b:")
 b = float(input())
 print("enter the c:")
-c = float(input()) #enter the variables
+c = float(input())
+
+# enter the variables
+
 D = b*b - 4*a*a
-x1 = (-b+sqrt(D))/2*a #calculation x1
-x2 = (-b-sqrt(D))/2*a #calculation x2
+x1 = (-b+sqrt(D))/2*a
+
+# calculation x1
+
+x2 = (-b-sqrt(D))/2*a
+
+# calculation x2
+
 print("x1 = ", x1, "x2 = ", x2)
