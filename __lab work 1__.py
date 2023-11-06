185f1dae548cce8f81b9b3f94558f2ba3d6f36cb
Andrey <pustakovandrej@gmail.com>
2023-11-06 21:59:32 +0300

    add programm

--- __lab work 1__.py
+++ __lab work 1__.py
@@ -4,3 +4,7 @@ print("enter the a, b, c:")
 a = float(input())
 b = float(input())
 c = float(input())
+D = b*b - 4*a*a
+x1 = (-b+sqrt(D))/2*a
+x2 = (-b-sqrt(D))/2*a
+print("x1 = ", x1, "x2 = ", x2)
