68d3f793b1d5f96ebd767656c6e80096c702f3c7
Andrey <pustakovandrej@gmail.com>
2023-11-06 22:12:36 +0300

    adding the comments

--- __lab work 1__.py
+++ __lab work 1__.py
@@ -1,12 +1,12 @@
 # the program for solving the quadratic equation
-from math import sqrt
+from math import sqrt #using libraries
 print("enter the a:")
 a = float(input())
 print("enter the b:")
 b = float(input())
 print("enter the c:")
-c = float(input())
+c = float(input()) #enter the variables
 D = b*b - 4*a*a
-x1 = (-b+sqrt(D))/2*a
-x2 = (-b-sqrt(D))/2*a
+x1 = (-b+sqrt(D))/2*a #calculation x1
+x2 = (-b-sqrt(D))/2*a #calculation x2
 print("x1 = ", x1, "x2 = ", x2)
